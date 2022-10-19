81e94780f23619d43345452e05c49039408e38ac
huangfeifan <huangfeifan@ones.ai>
2022-10-19 19:06:43 +0800

    feat: init button 🔘

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
 </style>
 
 <template>
-
+  <BMButton>123132123</BMButton>
 </template>
 
 <script setup lang="ts">
+import {BMButton} from './components/Button'
 </script>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,14 @@
+<style scoped>
+
+</style>
+
+<template>
+  <button><slot/></button>
+</template>
+
+<script lang="ts" setup>
+import {reactive, ref} from 'vue'
+
+const a = ref(0)
+
+</script>
